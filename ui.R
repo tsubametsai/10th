04b1941a5b2ca71e676d2663ba054e387a245641
tsubametsai <36106518+tsubametsai@users.noreply.github.com>
2018-06-15 00:39:13 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,53 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+### Data Science Capstone : Course Project
+### ui.R file for the Shiny app
+### Github repo : https://github.com/tsubametsai/10th
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+shinyUI(navbarPage("Capstone: Course Project",
+                   tabPanel("Predict the Next Word",
+                            HTML("<strong>Author: TSAI YI-FAN(National Chi Nan University,Dept,of International Business Studies)</strong>"),
+                            br(),
+                            HTML("<strong>Date: 2018/06/13</strong>"),
+                            br(),
+                            img(src = "headers.png"),
+                            # Sidebar
+                            sidebarLayout(
+                              sidebarPanel(
+                                helpText("Enter a partially complete sentence to begin the next word prediction"),
+                                textInput("inputString", "Enter a partial sentence here",value = ""),
+                                br(),
+                                br(),
+                                br(),
+                                br()
+                              ),
+                              mainPanel(
+                                h2("Predicted Next Word"),
+                                verbatimTextOutput("prediction"),
+                                strong("Sentence Input:"),
+                                tags$style(type='text/css', '#text1 {background-color: rgba(127.0.0.1:7553); color: blue;}'), 
+                                textOutput('text1'),
+                                br(),
+                                strong("Note:"),
+                                tags$style(type='text/css', '#text2 {background-color: rgba(127.0.0.1:7553); color: red;}'),
+                                textOutput('text2')
+                              )
+                            )
+                            
+                   ),
+                   tabPanel("About",
+                            mainPanel(
+                              img(src = "./headers.png"),
+                              includeMarkdown("about.md")
+                            )
+                   )
+)
+)
